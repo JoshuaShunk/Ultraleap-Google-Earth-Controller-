19cf911683906b28a725f1e7821dc17feacc17de
Joshua Shunk <hockeyshunk04@gmail.com>
2023-08-15 10:42:44 -0700

    Refactored behaviors to classes, updated README

---
+++ Controllers/ControllerConfig.py
@@ -0,0 +1,27 @@
+class ControllerConfig(object):
+    def __init__(self, name, behavior_func):
+        self.name = name
+        self.behavior_func = behavior_func
+
+    def execute_behavior(self, frame):
+        self.behavior_func(frame)
+
+class ConfigurationManager(object):
+    def __init__(self):
+        self.configs = {}
+        self.selected_config = None
+
+    def add_config(self, config):
+        if config.name in self.configs:
+            raise ValueError("Configuration with name '{}' already exists.".format(config.name))
+        self.configs[config.name] = config
+
+    def select_config(self, name):
+        if name not in self.configs:
+            raise ValueError("Configuration with name '{}' does not exist.".format(name))
+        self.selected_config = self.configs[name]
+
+    def execute_selected_behavior(self, frame):
+        if self.selected_config:
+            self.selected_config.execute_behavior(frame)
+
